1cde1bc07ad7c45869572fd0ffbec2424ec383e8
toris <ironjustlikethat@gmail.com>
2024-03-12 16:31:48 +0900

    Fix: 블로그 글 생성 버그 해결

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Post, PostRequest } from '@/types';
 import { createClient } from '@/utils/supabase/server';
-import { StorageError } from '@supabase/storage-js';
+import type { StorageError } from '@supabase/storage-js';
 import formidable from 'formidable';
 import { readFileSync } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -10,15 +10,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Post | StorageError>
 ) {
+  if (req.method !== 'POST') return res.status(405).end();
+
   const form = formidable();
   const [fields, files] = await form.parse(req);
   let preview_image_url: string | null = null;
-  const supabase = await createClient(req.cookies);
+  const supabase = createClient(req.cookies);
+
   if (files.preview_image?.length === 1) {
     const file = files.preview_image[0];
-    const fileContent = await readFileSync(file.filepath);
+    const fileContent = readFileSync(file.filepath);
     const fileName = `${file.newFilename}_${file.originalFilename}`;
-    const { data: uploadDat, error } = await supabase.storage
+    const { data: uploadData, error } = await supabase.storage
       .from('blog-image')
       .upload(fileName, fileContent, {
         contentType: file.mimetype ?? undefined
@@ -26,13 +29,14 @@ export default async function handler(
     if (error) {
       res.status(403).json(error);
     }
-    if (uploadDat?.path) {
+    if (uploadData?.path) {
       const { data } = supabase.storage
         .from('blog-image')
-        .getPublicUrl(uploadDat.path);
+        .getPublicUrl(uploadData.path);
       preview_image_url = data.publicUrl;
     }
   }
+
   const { title, category, tags, content } = fields;
 
   const postRequest = {
@@ -46,7 +50,16 @@ export default async function handler(
   const { data } = await supabase.from('Post').insert([postRequest]).select();
 
   if (data && data.length === 1) {
-    const { tags, ...rest } = data[0];
-    res.status(200).json({ ...rest, tags: JSON.parse(tags) as string[] });
-  } else res.status(500).end;
+    const { tags, ...reset } = data[0];
+    res.status(200).json({
+      ...reset,
+      tags: JSON.parse(tags) as string[]
+    });
+  } else res.status(500).end();
 }
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
