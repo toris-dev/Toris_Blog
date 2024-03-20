a2e7250a2adda38aa83d84f7be947a40ad56308e
toris <ironjustlikethat@gmail.com>
2024-03-20 16:15:01 +0900

    ♻️ Refactor : SSR -> SSG 변경, react-query initialData 을 getStaticProps 초기데이터 세팅

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,3 +1,4 @@
+import { Post } from '@/types';
 import { cn } from '@/utils/style';
 import { createClient } from '@/utils/supabase/client';
 import { useInfiniteQuery } from '@tanstack/react-query';
@@ -11,9 +12,15 @@ type PostListProps = {
   category?: string;
   tag?: string;
   className?: string;
+  initalPosts?: Post[];
 };
 
-const PostList: FC<PostListProps> = ({ category, tag, className }) => {
+const PostList: FC<PostListProps> = ({
+  category,
+  tag,
+  className,
+  initalPosts
+}) => {
   const { ref, inView } = useInView();
 
   const {
@@ -29,7 +36,7 @@ const PostList: FC<PostListProps> = ({ category, tag, className }) => {
       if (tag) request = request.like('tags', `%${tag}%`);
       const { data } = await request
         .order('created_at', { ascending: false })
-        .range(pageParam, pageParam + 9);
+        .range(pageParam, pageParam + 4);
 
       if (!data)
         return {
@@ -37,10 +44,24 @@ const PostList: FC<PostListProps> = ({ category, tag, className }) => {
           nextPage: null
         };
       return {
-        posts: data,
-        nextPage: data.length === 10 ? pageParam + 10 : null
+        posts: data.map((post) => ({
+          ...post,
+          tags: JSON.parse(post.tags) as string[]
+        })),
+        nextPage: data.length === 5 ? pageParam + 5 : null
       };
     },
+    initialData: !!initalPosts
+      ? {
+          pages: [
+            {
+              posts: initalPosts,
+              nextPage: initalPosts.length === 5 ? 5 : null
+            }
+          ],
+          pageParams: [0]
+        }
+      : undefined,
     initialPageParam: 0,
     getNextPageParam: (lastPage) => lastPage.nextPage
   });
