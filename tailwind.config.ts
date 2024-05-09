0ce5268ff2802d87b8934d581f7b63acecc1a958
toris-dev <ironjustlikethat@gmail.com>
2024-05-09 15:32:37 +0900

    ➕ Chore: toast 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,15 +30,17 @@ const config: Config = {
         '90/100': '90%',
         '100/100': '100%'
       },
-      keyframes: {
-        shake: {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '25%, 75%': { transform: 'translateX(-10px)' },
-          '50%': { transform: 'translateX(10px)' }
-        }
-      },
       animation: {
-        shake: 'shake 0.5s cubic-bezier(.36,.07,.19,.97) both'
+        'spin-slower': 'spin 35s ease infinite',
+        'spin-slow': 'spin 25s ease-in-out infinite reverse'
+      },
+      colors: {
+        accent: {
+          1: 'hsl(288 95.8% 90.6%)',
+          2: 'hsl(168 83.8% 78.2%)'
+        },
+        bkg: 'hsl(210 40% 98%)',
+        content: 'hsl(217 32.6% 17.5%)'
       }
     }
   },
