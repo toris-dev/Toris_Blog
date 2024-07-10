ccb2e05b3e7d4c0dd0ebd0d3824d026999796f1a
toris-dev <ironjustlikethat@gmail.com>
2024-07-10 16:26:46 +0900

    🐛 Fix : 댓글 supabase 가져오는 post API 수정

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -1,5 +1,13 @@
 import { createClient } from '@/utils/supabase/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+// export async function GET(request: NextRequest) {
+//   try {
+
+//   } catch (error) {
+//     return NextResponse.json({ message: error }, { status: 404 });
+//   }
+// }
 
 export async function POST(request: NextRequest) {
   try {
@@ -19,11 +27,11 @@ export async function POST(request: NextRequest) {
       .order('created_at', { ascending: false });
     if (error) {
       console.log(error);
-      return NextResponse.json({ message: error }, { status: 404 });
+      return Response.json({ message: error }, { status: 404 });
     }
-    return NextResponse.json(data);
+    return Response.json(data[0]);
   } catch (error) {
-    return NextResponse.json({ message: '댓글이 없습니다.' }, { status: 500 });
+    return Response.json({ message: '댓글이 없습니다.' }, { status: 500 });
   }
 }
 
@@ -36,24 +44,44 @@ export async function DELETE(request: NextRequest) {
       .delete()
       .match({
         id: Number(comment_id),
-        writer_id: writer_id,
         password: password
       })
       .select('*');
     if (data?.length !== 1) {
-      return NextResponse.json({
+      return Response.json({
         message: '댓글을 삭제하지 못했습니다.',
         error: true
       });
     }
-    return NextResponse.json({
-      message: '댓글이 삭제되었습니다.',
-      error: false
-    });
+    return Response.json(data[0]);
   } catch (error) {
-    return NextResponse.json(
+    return Response.json(
       { message: '댓글을 삭제하지 못했습니다.', error: true },
       { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(request: NextRequest) {
+  try {
+    const supabase = createClient();
+    const { id, pwd, content } = await request.json();
+    const { data, error } = await supabase
+      .from('Comments')
+      .update({ content })
+      .match({ writer_id: id, password: pwd })
+      .select('*');
+
+    if (error) {
+      return Response.json({ message: error }, { status: 404 });
+    }
+    return Response.json(data[0]);
+  } catch (error) {
+    Response.json(
+      {
+        message: '댓글을 업데이트 하지 못했습니다.'
+      },
+      { status: 404 }
     );
   }
 }
